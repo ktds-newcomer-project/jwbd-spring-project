b2f9629cd6d39ac2b65fd91c88b84ba6313135a8
DESKTOP-C70T359\SEO <seojiwon335@gmail.com>
2022-08-10 17:46:19 +0900

    Create Problem Repository

---
+++ BE/src/main/java/com/ktds/kxam/repo/ProblemRepo.java
@@ -0,0 +1,25 @@
+package com.ktds.kxam.repo;
+
+import com.ktds.kxam.entity.Problem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+
+@Repository
+public interface ProblemRepo extends JpaRepository<Problem, Long> {
+    @Modifying
+    @Transactional
+    @Query("update Problem p set p.answer =:answer where p.pid = :pid ")
+    void updateAnswer(@Param("pid") Long pid, @Param("answer") String answer);
+
+    @Modifying
+    @Transactional
+    @Query("update Problem p set p.items =:items where p.pid = :pid ")
+    void updateItems(@Param("pid") Long pid, @Param("items") String items);
+
+    List<Problem> findProblemsByTestId(Long testId);
+}
