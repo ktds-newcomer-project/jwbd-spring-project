3f969c0aa415b1512298cc903110921e2ee6a939
DongKyunJung <strawberry01128@gmail.com>
2022-08-12 16:54:10 +0900

    update// TestPage[Clock, TestRatio push]

--- FE/src/pages/TestPage/index.jsx
+++ FE/src/pages/TestPage/index.jsx
@@ -15,37 +15,39 @@ const TestPage = () => {
   const [sec, setSec] = useState();
   const [min, setMin] = useState();
   const [hour, setHour] = useState();
+  const [arr, setArr] = useState([]);
+
+  
   useEffect(() => {
+    setArr(Array(questions.length).fill())
     const id = setInterval(() => {
       let date1 = moment();
-      let date2 = moment("2022-08-11T18:50:00+09:00");
+      let date2 = moment("2022-08-12T17:29:00+09:00");
       date1.format(); // 2021-10-09T00:44:52+09:00
       date2.format(); // 2020-04-08T00:00:00+09:00
       const dateDis = date2.diff(date1)
       let dis_Hour = 0
       let dis_Minute = 0
       let dis_Seconds = 0
-      if (dateDis >= 3600) {
+      if (dateDis >= 0) {
         dis_Hour = date2.diff(date1, 'hours')
         dis_Minute = date2.diff(date1, 'minutes') - (dis_Hour * 60)
         dis_Seconds = date2.diff(date1, 'seconds') - (dis_Hour * 3600 ) - (dis_Minute * 60)
         setSec(dis_Seconds)
         setMin(dis_Minute)
         setHour(dis_Hour)
+        console.log(dateDis)
+      }else{
+        clearInterval(id);
+        Swal.fire("시간이 종료되었습니다")
+        navigate('/home')
       }
-      console.log(dis_Hour,dis_Minute,dis_Seconds)
-      console.log(dateDis)
       setDateDis(dateDis => dateDis - 1);
       
-      console.log()
+
     }, 1000);
-    if(dateDis <= 0){
-      clearInterval(id);
-      Swal.fire("시간이 종료되었습니다")
-      navigate('/home')
-    }
     return () => clearInterval(id);
-  }, [dateDis]);
+  }, []);
 
   const testList = [
     { label: 'JAVA WEB 테스트', testCode: 82256975 },
@@ -59,27 +61,19 @@ const TestPage = () => {
 
   const questions = [
                     {type: 1, question:"아이스크림을 좋아하나요?", bogi : []},
-                    {type: 0, question:"현직이형을 좋아하나요?", bogi : []},
+                    {type: 0, question:"현직이형을 좋아하나요?", bogi : ['매우 좋아한다.','조금 좋아한다.', '보통이다.', '조금 혐오한다.', '혐오한다.']},
                     {type: 1, question:"병훈이형을 좋아하나요?", bogi : []},
-                    {type: 0, question:"지원이를 좋아하나요?", bogi : []},
+                    {type: 0, question:"지원이를 좋아하나요?", bogi : ['매우 좋아한다.','조금 좋아한다.', '보통이다.', '조금 혐오한다.', '혐오한다.']},
                     {type: 1, question:"태형이를 좋아하나요?", bogi : []},
-                    {type: 0, question:"정동균을 좋아하나요?", bogi : []}
-                    ]
-  // let banana = 0
-  // const questionBox = questions.map((item) => {
-  //   banana += 1
-  //   return <div>
-  //     {banana}
-  //     <p> {item.question}</p>
-  //   </div>
-    
-  // })
-
+                    {type: 1, question:"정동균을 좋아하나요?", bogi : []}
+                    ]       
     return (
        <div className="test-page-div">
           <h1>KT DS Test</h1>
           <h1 className="timer"> 남은 시간  : {hour} {min} {sec}</h1>  
-          <QuestionCard questions={questions} />
+          <div style={{width: "40%"}}>
+            <QuestionCard questions={questions} arr={arr} />
+            </div>
           <div className="test-div">
             <Button style={{ marginLeft : "2vw", height:"40px"}} variant="outlined" size="small">제출하기</Button>
           </div>
--- FE/src/templates/QuestionCard/index.jsx
+++ FE/src/templates/QuestionCard/index.jsx
@@ -1,16 +1,71 @@
-import React from 'react';
-const QuestionCard = (questions) => {
-    console.log(questions)
+import React, { useEffect, useState } from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+
+const QuestionCard = (questions,arr) => {  
+    useEffect(() => {
+        console.log(questions.arr)
+    })
+    let CheatKey = false
+    const handleChange = (event) => {
+        
+        console.log(event.target.value)
+        questions.arr[event.target.name - 1] = event.target.value
+        console.log(event.target)
+        console.log(questions.arr)
+    }
+    const choiquestion = (temtem,banana) => temtem.map((tem) => {
+        
+        apple += 1
+        if (apple === temtem.length-1){
+            CheatKey = true
+        }
+        let sentense =  apple + '. ' + tem
+        return <div>
+        <FormControlLabel value={apple} banana={banana} control={<Radio />} label={sentense} />
+        </div>
+    }) 
+
     let banana = 0
+    let apple = 0
+    
     return (
         <div>
             {questions.questions.map((item) => {
                 banana += 1
-                return <div>
+                if (CheatKey) {
+                    apple = 0
+                    CheatKey = false
+                }
+                return <div style={{marginBottom:"20%"}}>
+                { (item.type === 1) ?
+                <>
+                <p>Q{banana}.{item.question}</p>
+                <textarea style={{height:"100px",width:"100%"}}/>
+                </>
+                :
+                <>
+                
+                <p>Q{banana}.  {item.question}</p>
+                <FormControl>
+                    <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="female"
+                    name={banana}
+                    onChange={handleChange}
+                    >
 
-                <p>{banana} {item.question}</p>
-                <textarea></textarea>
+                        {choiquestion(item.bogi,banana)}
+                    </RadioGroup>
+                </FormControl>
+                </>
+                }
                 </div>
+                
             })}
         </div>
     );
