64dd0b0446d483c048c086a6aa5efdc6e54cb92f
CodeBottle <89330527+codebottlehun@users.noreply.github.com>
2022-08-19 06:23:42 +0900

    update terminated-proxy

--- FE-admin/src/setupProxy.js
+++ FE-admin/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = (app) => {
-  app.use(
-    createProxyMiddleware("/api", {
-      target: "http://localhost:8080",
-      changeOrigin: true,
-    })
-  );
-};
+// module.exports = (app) => {
+//   app.use(
+//     createProxyMiddleware("/api", {
+//       target: "http://localhost:8080",
+//       changeOrigin: true,
+//     })
+//   );
+// };
