8e7e9c77a6e283f3b7b69fd52081504cdd281dc8
CodeBottle <89330527+codebottlehun@users.noreply.github.com>
2022-08-19 10:09:38 +0900

    update proxy

--- FE-admin/src/setupProxy.js
+++ FE-admin/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://144.24.91.7:8080",
+      target: "http://localhost:8080",
       changeOrigin: true,
     })
   );
